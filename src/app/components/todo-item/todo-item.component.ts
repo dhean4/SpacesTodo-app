43967d4d9093569fadd27017a04b4b80973ad11c
Daniel Ibisagba <ida4life2003@gmail.com>
2023-11-28 02:38:10 +0100

    app component structuring

---
+++ src/app/components/todo-item/todo-item.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+@Component({
+  selector: 'app-todo-item',
+  standalone: true,
+  imports: [CommonModule],
+  templateUrl: './todo-item.component.html',
+  styleUrl: './todo-item.component.scss'
+})
+export class TodoItemComponent {
+
+}
